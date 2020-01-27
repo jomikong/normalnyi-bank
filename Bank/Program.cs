aaf70e3d1f7b7a31bf1151a5af47dbe8ccc5158a
jomikong <vaniakozmenko@yandex.ru>
2020-01-27 16:34:55 +0300

    Клавиша

---
+++ Bank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Bank
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            char key = Console.ReadKey(true).KeyChar;
+            if (key == 'w') { 
+            
+            } else if(key == 's')
+            {
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
